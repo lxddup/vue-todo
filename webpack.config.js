d0c514928e221f8d2b7da1c4bdf14919a5211ce9
nododo <nododo1027@gmail.com>
2019-08-09 16:12:25 +0800

    配置webpack loader

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+module.exports = {
+   entry:path.join(__dirname,'src/index.js'),
+   output:{
+       filename:'bundle.js',
+       path:path.join(__dirname,'dist')
+   },
+   plugins: [
+    // make sure to include the plugin for the magic
+    new VueLoaderPlugin()
+],
+   module:{
+       rules:[
+           {
+               test:/\.vue$/,
+               loader:'vue-loader'
+           },
+           {
+               test:/\.css$/,
+            //    loader:'css-loader'
+            use:[
+                'style-loader',
+                'css-loader'
+            ]
+           },
+           {
+              test: /\.styl/,
+              use:[
+                  'style-loader',
+                  'css-loader',
+                  'stylus-loader'
+              ]
+           },
+           {
+               test:/\.(gif|jpg|jpeg|png|svg)$/,
+               use:[
+                   {
+                       loader:'url-loader',
+                       options:{
+                           limit: 1024,
+                           name: '[name]-aaa.[ext]'
+                       }
+                   }
+               ]
+           }
+       ]
+   }
+}
