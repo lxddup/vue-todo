147d6fbfef2ac139895311b361d47899e412946d
nododo <nododo1027@gmail.com>
2019-08-10 16:00:59 +0800

    webpack配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,84 @@
 const path = require('path')
+const HTMLPlugin = require('html-webpack-plugin')
+const isDev  = process.env.NODE_ENV === 'development'
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
-module.exports = {
-   entry:path.join(__dirname,'src/index.js'),
-   output:{
-       filename:'bundle.js',
-       path:path.join(__dirname,'dist')
-   },
-   plugins: [
-    // make sure to include the plugin for the magic
-    new VueLoaderPlugin()
-],
-   module:{
-       rules:[
-           {
-               test:/\.vue$/,
-               loader:'vue-loader'
-           },
-           {
-               test:/\.css$/,
-            //    loader:'css-loader'
-            use:[
-                'style-loader',
-                'css-loader'
-            ]
-           },
-           {
-              test: /\.styl/,
-              use:[
-                  'style-loader',
-                  'css-loader',
-                  'stylus-loader'
-              ]
-           },
-           {
-               test:/\.(gif|jpg|jpeg|png|svg)$/,
-               use:[
-                   {
-                       loader:'url-loader',
-                       options:{
-                           limit: 1024,
-                           name: '[name]-aaa.[ext]'
-                       }
-                   }
-               ]
-           }
-       ]
-   }
+const webpack = require('webpack')
+const config = {
+    target: 'web',
+    entry: path.join(__dirname, 'src/index.js'),
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'dist')
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.css$/,
+                //    loader:'css-loader'
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.styl/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'stylus-loader'
+                ]
+            },
+            {
+                test: /\.(gif|jpg|jpeg|png|svg)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 1024,
+                            name: '[name]-aaa.[ext]'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    // plugins: [
+    //     // make sure to include the plugin for the magic
+    //     //
+    //     new VueLoaderPlugin()
+    // ],
+    plugins:[
+        new VueLoaderPlugin(),
+        new webpack.DefinePlugin({
+            'process.env':{
+                NODE_ENV: isDev ? '"development"' : '"production"'
+            }
+        }),
+        new HTMLPlugin()
+    ]
 }
+if(isDev){
+    config.devtool = '#cheap-module-eval-source-map'
+    config.devServer ={
+          port:8000,
+          host:'0.0.0.0',
+          overlay:{
+              errors:true,
+          },
+          open:true,
+        //   historyFallback:{
+            
+        //   }
+        hot:true
+    }
+    config.plugins.push(
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin()
+    )
+}
+module.exports = config
